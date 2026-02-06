eef389692419b5583d2a93bb12897d73a0beb332
OLEG <oskaryahoo@gmail.com>
2026-02-06 21:42:23 +0300

    Initial commit

---
+++ src/main/java/com/oskarvos/cityweatherapp/CityWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.oskarvos.cityweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWeatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityWeatherAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oskarvos/cityweatherapp/CityWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarvos.cityweatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityWeatherAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
