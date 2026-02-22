f46b7b3e64581f2eaf65beb93c25857bb8fae838
oskarvos <oskaryahoo@gmail.com>
2026-02-22 20:17:34 +0300

    feat: добавлен новый модуль weather-concumer

---
+++ weather-consumer/src/main/java/com/oskarvos/weatherconsumer/WeatherConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.oskarvos.weatherconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherConsumerApplication.class, args);
+    }
+
+}
