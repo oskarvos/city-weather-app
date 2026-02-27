b97248331827275d6bb24b0f6eda443e779ee02c
oskarvos <oskaryahoo@gmail.com>
2026-02-27 17:01:42 +0300

    fix: @JsonInclude(JsonInclude.Include.NON_NULL) в CityRespons

--- src/main/java/com/oskarvos/cityweatherapp/city/dto/response/CityResponse.java
+++ src/main/java/com/oskarvos/cityweatherapp/city/dto/response/CityResponse.java
@@ -1,6 +1,7 @@
 package com.oskarvos.cityweatherapp.city.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({"id", "cityName", "temperature"})
 public class CityResponse {
 
